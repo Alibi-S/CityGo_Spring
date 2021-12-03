97606f1c69dd4df02f3edcf5c4a63624997404b8
Alibi <alibisemeikhan@gmail.com>
2021-12-03 22:32:05 +0600

    Front with RestTeplate and Zuul | get Logs

--- front-service/src/main/java/com/example/frontservice/controller/IndexController.java
+++ front-service/src/main/java/com/example/frontservice/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.example.frontservice.controller;
 
 
+import com.example.frontservice.model.DTO.LogListDTO;
+import com.example.frontservice.model.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
 //    @Autowired
 //    private MessageRepo messageRepo;
@@ -22,4 +31,27 @@ public class IndexController {
         return "main";
     }
 
+    @GetMapping("/login")
+    public String autorization(Map<String, Object> model) {
+        return "autorization";
+    }
+
+    @GetMapping("/registration")
+    public String registration(Map<String, Object> model) {
+        return "registration";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+//        LogListDTO logList = restTemplate.getForObject("http://logging-api/logs/dto", LogListDTO.class);
+
+        LogListDTO logList = restTemplate.getForObject("http://zuul-service/logs/dto", LogListDTO.class);
+
+
+        System.out.println(logList.getLogs().toString());
+
+        model.addAttribute(logList.getLogs());
+
+        return "adminpage";
+    }
 }
--- front-service/src/main/java/com/example/frontservice/controller/MainPageController.java
+++ front-service/src/main/java/com/example/frontservice/controller/MainPageController.java
@@ -1,27 +1,39 @@
-package com.example.frontservice.controller;
-
-import com.example.frontservice.model.Answer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-@Controller
-public class MainPageController {
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @RequestMapping(value="/main", method=RequestMethod.GET)
-    public String greetingForm(Model model) {
-        model.addAttribute("answer", new Answer());
-        return "main";
-    }
-
-    @RequestMapping(value="/main", method=RequestMethod.POST)
-    public String greetingSubmit(@ModelAttribute Answer answer, Model model) {
-        model.addAttribute("greeting", answer);
-        return "main";
-    }
-}
+//package com.example.frontservice.controller;
+//
+//import com.example.frontservice.model.Answer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.Map;
+//
+//@Controller
+//public class MainPageController {
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    @RequestMapping(value="/main", method=RequestMethod.GET)
+//    public String greetingForm(Model model) {
+//        model.addAttribute("answer", new Answer());
+//        return "main";
+//    }
+//
+//    @RequestMapping(value="/main", method=RequestMethod.POST)
+//    public String greetingSubmit(@ModelAttribute Answer answer, Model model) {
+//        model.addAttribute("greeting", answer);
+//        return "main";
+//    }
+//
+//    @RequestMapping(value="/main", method=RequestMethod.GET)
+//    public String autorization(Map<String, Object> model/*Model model*/) {
+//
+//        //model.addAttribute("name", name);
+//        //model.put("name", name);
+//        return "autorization";
+//    }
+//
+//
+//}
