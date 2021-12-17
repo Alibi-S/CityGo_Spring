67b3e6d23903a9abdbb2ad5ae3f7ef4a575bf2b7
Alibi <alibisemeikhan@gmail.com>
2021-12-17 22:36:58 +0600

    Adding RabbitMQ dependencies

---
+++ logging-api/src/main/java/com/example/loggingapi/messaging/RabbitMqListener.java
@@ -0,0 +1,27 @@
+package com.example.loggingapi.messaging;
+
+import com.example.loggingapi.model.Log;
+import com.example.loggingapi.service.LogService;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@EnableRabbit
+@Component
+public class RabbitMqListener {
+    @Autowired
+    public LogService logService;
+
+    @RabbitListener(queues = "queue1")
+    public void processQueue1(String message) {
+        System.out.println("Received from queue 1: " + message);
+        String[] splited = message.split("\\s+");
+        Log log = new Log();
+        log.setUserId(Long.parseLong(splited[0]));
+        log.setService(splited[1]);
+        log.setAction(splited[2]);
+        log.setDescription(splited[3]);
+        logService.CreateLog(log);
+    }
+}
